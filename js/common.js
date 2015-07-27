01030e9f0a21fe819cf99681ac04f6ff3c376976
veremey <veremey@i.ua>
2015-07-28 01:30:24 +0300

    strt

--- js/common.js
+++ js/common.js
@@ -15,31 +15,57 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	// slick carousel
 
-	$('.js-carousel').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		fade: true,
-		asNavFor: '.js-carousel-preview',
-	});
-	$('.js-carousel-preview').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		asNavFor: '.js-carousel',
-		dots: false,
-		arrows: true,
-		infinite: false,
-		centerMode: false,
-		focusOnSelect: true,
-	});
+	$(document).ready(function() {
+		// slick carousel
 
-	$(".js-carousel-preview .slick-slide").on("click",function (){
-	  $(this).parent().find(".slick-slide").removeClass("is-active");
-	  $(this).addClass("is-active")
-	  return false;
-	});
+		$('.js-carousel').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			fade: true,
+			asNavFor: '.js-carousel-preview',
+		});
+		$('.js-carousel-preview').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			asNavFor: '.js-carousel',
+			dots: false,
+			arrows: true,
+			infinite: false,
+			centerMode: false,
+			focusOnSelect: true,
+		});
+
+		$(".js-carousel-preview .slick-slide").on("click",function (){
+		  $(this).parent().find(".slick-slide").removeClass("is-active");
+		  $(this).addClass("is-active")
+		  return false;
+		});
+		// end slick carousel
+
+		// выбор типа метала
+		$('.metal').click(function() {
+			$(this).siblings().find('.is_active').removeClass('is_active');
+			$(this).children('.metal__img').addClass('is_active');
+		});
+
+		// polzunok
 
 
-});
+
+		//zoom picture
+
+		$("#zoom").elevateZoom({
+			zoomType: "lens",
+			lensShape : "round",
+			lensSize : 200,
+			borderSize: 2,
+			borderColour: "#4fade3",
+			lensSize: 110
+		});
+
+
+	});// end doc ready
+
+}); // end head
--- js/init.js
+++ js/init.js
@@ -1,4 +1,6 @@
 head.load("js/lib/jquery.js",
 					"js/lib/cycle.js",
 					"js/lib/slick.min.js",
+					"js/jquery-ui-1.10.3.custom.js",
+					"js/jquery.elevateZoom-3.0.8.min.js",
 					"js/common.js");
