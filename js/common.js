8847e14f70a7a7ad6303dbbc24ea623229e00e06
veremey <veremey@i.ua>
2015-07-30 22:04:20 +0300

    select

--- js/common.js
+++ js/common.js
@@ -108,8 +108,50 @@ head.ready(function() {
 		}
 		ui_slider();
 
+		$(document).click(function() {
+			 $(".js-drop ul").hide();
+			 $(".js-select-list").hide();
+			 $(".js-select").removeClass("is-active");
+			});
 
 
+
+		// check select
+
+		function select() {
+			$(".js-select").each(function(){
+				var select_list = $(this).parent().find(".js-select-list");
+				var text = select_list.find("li").first().text();
+				$(this).find(".js-select-text").text(text);
+				$(this).click(function(event){
+					if ($(this).hasClass("is-active")) {
+					    $(this).removeClass("is-active");
+					    select_list.slideUp("fast");
+					}
+					else {
+					    $(".js-select").removeClass("is-active");
+					    $(".js-select-list").hide();
+					    select_list.slideDown("fast");
+					    $(this).addClass("is-active");
+					}
+					event.stopPropagation();
+				});
+				select_list.find("li").click(function(event) {
+					var id = $(this).attr("data-id");
+					var text = $(this).text();
+					$(this).parent().parent().find(".js-select-text").text(text);
+					$(this).parent().parent().find(".js-select-input").val(id);
+					$(this).parent().hide();
+					$(this).parents(".js-select").removeClass("is-active");
+					event.stopPropagation();
+				});
+			});
+		}
+		select();
+
+		$('.js-select').click(function(event){
+		    event.stopPropagation();
+		});
 
 		//zoom picture
 
